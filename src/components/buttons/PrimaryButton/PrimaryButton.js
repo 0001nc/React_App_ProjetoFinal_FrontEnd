27f0131e7ee9b189078ce9ef3012689e3805f7a6
Nuno Costa <128308683+0001nc@users.noreply.github.com>
2023-12-22 18:38:58 +0000

    Delete src directory

--- src/components/buttons/PrimaryButton/PrimaryButton.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./PrimaryButton.scss";
-
-export default function PrimaryButton(props) {
-    return <div className={"PrimaryButton"}>
-        <button> {props.text}</button>
-    </div>
-}
--- src/components/buttons/SecondaryButton/SecondaryButton.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./SecondaryButton.scss"
-
-export default function SecondaryButton(props) {
-    return <div className={"SecondaryButton"}>
-        <button className="button"> {props.text}</button>
-    </div>
-}
